298fd623150fad939f70ea48fe4ce66d195efcb6
Maria De Sousa Ardiet <mdg.desousa@gmail.com>
2022-11-29 10:08:51 +0100

    Doctrine relation ManyToOne on Program with Catetgory

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -23,6 +23,10 @@ class Program
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $poster = null;
 
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -60,6 +64,18 @@ class Program
     public function setPoster(?string $poster): self
     {
         $this->poster = $poster;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
